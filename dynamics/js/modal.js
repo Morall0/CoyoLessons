9f3ce876f5f4f45a36662da40765108026871516
Maurici0ML <alanmauriciomoraleslopez@gmail.com>
2021-06-06 16:27:49 -0500

    FEAT: regex en js;

--- dynamics/js/modal.js
+++ dynamics/js/modal.js
@@ -1,5 +1,5 @@
 $(document).ready(()=>{
-
+    
     function peticion(url, data=""){
         let peticion =$.ajax({
             url: url,
@@ -9,6 +9,64 @@ $(document).ready(()=>{
         return peticion;
     };
 
+
+    function verifRegx(num_cuenta, nombre, apPaterno, apMaterno, correo, tel, contra){
+        //Variables REGEX
+        let regexNumcuenta = /^((3(19|20|21))|(1(16|17|18)))\d{6}$/;
+        let regexNombre = /([A-Z,a-z,Ññ,á,é,í,ó,ú,Á,É,Í,Ó,Ú](\s)?){2,30}/;
+        let regexApellidos = /^([A-Z,a-z,Ññ,á,é,í,ó,ú,Á,É,Í,Ó,Ú]){2,30}$/;
+        let regexCorreo = /^[\w\.\-]{4,28}@(((g|hot)mail|outlook|live|yahoo)\.com|(comunidad|alumno\.enp|enp)\.unam\.mx)|\.mx$/;
+        let regexTel = /^((55|56)(\d{8}))$/;
+        let regexContra = /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[$#@$!=\(\)/%*?&])([A-Za-z\d$#@$!=\(\)/%*?&]|[^ ]){10,30}$/;
+
+        let cont=0;
+
+        if(regexNumcuenta.test(num_cuenta)==true){
+            cont++;
+        }else{
+            $("#numcuenta").attr("pattern", "^((3(19|20|21))|(1(16|17|18)))\\d{6}$");
+            $("#numcuenta").attr("title", "Introduce un número de cuenta válido");
+        }
+        if(regexNombre.test(nombre)==true){
+            cont++;
+        }else{
+            $("#nombre").attr("pattern", "([A-Z,a-z,Ññ,á,é,í,ó,ú,Á,É,Í,Ó,Ú](\\s)?){2,30}");
+            $("#nombre").attr("title", "Ingresa tu(s) nombre(s)");
+        }
+        if(regexApellidos.test(apPaterno)==true){
+            cont++;
+        }else{
+            $("#apPaterno").attr("pattern", "^([A-Z,a-z,Ññ,á,é,í,ó,ú,Á,É,Í,Ó,Ú]){2,30}$");
+            $("#apPaterno").attr("title", "Introduce tu apellido paterno");
+        }
+        if(regexApellidos.test(apMaterno)==true){
+            cont++;
+        }else{
+            $("#apMaterno").attr("pattern", "^([A-Z,a-z,Ññ,á,é,í,ó,ú,Á,É,Í,Ó,Ú]){2,30}$");
+            $("#apMaterno").attr("title", "Introduce tu apellido materno");
+        }
+        if(regexCorreo.test(correo)==true){
+            cont++;
+        }else{
+            $("#correo").attr("pattern", "^[\\w\\.\\-]{4,28}@(((g|hot)mail|outlook|live|yahoo)\\.com|(comunidad|alumno\\.enp|enp)\\.unam\\.mx)|\\.mx$");
+            $("#correo").attr("title", "Introduce un correo válido");
+        }
+        if(regexTel.test(tel)==true){
+            cont++;
+        }else{
+            $("#tel").attr("pattern", "^((55|56)(\\d{8}))$");
+            $("#tel").attr("title", "Introduce tu número telefónico");
+        }
+        if(regexContra.test(contra)==true){
+            cont++;
+        }else{
+            $("#contraseña").attr("pattern", "^(?=.*[a-z])(?=.*[A-Z])(?=.*\\d)(?=.*[$#@$!=\\(\\)/%*?&])([A-Za-z\\d$#@$!=\\(\\)/%*?&]|[^ ]){10,30}$");
+            $("#contraseña").attr("title", "Introduce una contraseña de 10 a 30 caracteres, con al menos un caracter especial, un número, una minúscula y una mayúscula");
+        }
+
+        return cont;
+    };
+    
     //Botones del modal (abrir y cerrar).
     $("#crearCuenta").click(()=>{
         $("#miModal").css("display", "block");
@@ -49,29 +107,35 @@ $(document).ready(()=>{
         let dsemana= $("#HorarioDia").val();
         let hora= $("#HorarioHora").val();
         let contra=$("#contraseña").val();
-        let respuestas = peticion('../dynamics/php/consultaModal.php', 'num_cuenta='+num_cuenta+'&nombre='
-        +nombre+' '+apPaterno+' '+apMaterno+'&correo='+correo+'&tel='+tel+'&fechaNac='+año+'-'+mes+'-'+dia+
-        '&cursando='+cursando+'&materias='+materias+'&dsemana='+dsemana+'&hora='+hora+'&contra='+contra);
 
-        respuestas.done((respuesta)=>{
-            alert("si se envian desde JS");
-            alert(respuesta);
-            if(respuesta == "REGISTRO EXITOSO"){
-                alert("location");
-            }
-            if(respuesta > 0){
-                $("#numcuenta").val("");
-                $("#numcuenta").attr("placeholder","Este numero de cuenta ya está registrado");
-                $("#numcuenta").addClass("placehold");
-            }
-            else{
-                $("#miModal").css("display", "none");
-            }
-        });
-        
-        respuestas.fail((respuesta)=>{
-            alert("NO se envian desde JS");
-        });
+        let regexing = verifRegx(num_cuenta, nombre, apPaterno, apMaterno, correo, tel, contra);
+
+        if(regexing == 7){
+            let respuestas = peticion('../dynamics/php/consultaModal.php', 'num_cuenta='+num_cuenta+'&nombre='
+            +nombre+' '+apPaterno+' '+apMaterno+'&correo='+correo+'&tel='+tel+'&fechaNac='+año+'-'+mes+'-'+dia+
+            '&cursando='+cursando+'&materias='+materias+'&dsemana='+dsemana+'&hora='+hora+'&contra='+contra);
+
+            respuestas.done((respuesta)=>{
+                alert("si se envian desde JS");
+                alert(respuesta);
+                if(respuesta > 0){
+                    $("#numcuenta").val("");
+                    $("#numcuenta").attr("placeholder","Este numero de cuenta ya está registrado");
+                    $("#numcuenta").addClass("placehold");
+                }
+                else{
+                    $("#miModal").css("display", "none");
+                }
+            });
+            
+            respuestas.fail((respuesta)=>{
+                alert("NO se envian desde JS");
+            });
+        }
+        else{
+            alert("Verificar que se hayan puesto correctamente en el html");
+        }
+
         
     });
 
